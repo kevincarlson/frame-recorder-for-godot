8882facc245343ff40fb5be4bd94bd4412d9fb01
Kevin Carlson <kdesltd@gmail.com>
2021-01-10 23:35:41 -0500

    Update camera3d.cs
    Added leading zeroes to file output
    Removed viewport resizing, as this seemed to be causing a crash
    Removed Standalone mode requirement for recording

--- camera3d.cs
+++ camera3d.cs
@@ -10,38 +10,37 @@
 using Godot;
 using System;
 
-public class camera : Camera
+public class camera3d : Camera
 {
-	bool standalone = OS.HasFeature("standalone");
+	[Export]
+	private String _outputDir = "user://render";
 	
 	public override void _Ready()
 	{
-		if (standalone) {
-			GetViewport().Msaa = Godot.Viewport.MSAA.Msaa8x;
-			GetViewport().Size = GetViewport().Size * 4;
-			Directory outputDir = new Directory();
-			outputDir.MakeDir("user://render");
-		}
+		GetViewport().Msaa = Godot.Viewport.MSAA.Msaa8x;
+		Directory outputDir = new Directory();
+		outputDir.MakeDir(_outputDir);
 	}
 
 	public override void _Process(float delta)
 	{
 		int framesDrawn = Engine.GetFramesDrawn();
 		
-		if (standalone && framesDrawn > 0) {
-			GD.Print("Rendering Frame " + framesDrawn + "..." );
-			
+		if (framesDrawn > 0)
+		{
 			GetViewport().RenderTargetClearMode = Godot.Viewport.ClearMode.OnlyNextFrame;
 			var image = GetViewport().GetTexture().GetData();
 			image.GenerateMipmaps();
-			image.Resize((int) GetViewport().Size.x / 4, (int) GetViewport().Size.y / 4, Image.Interpolation.Trilinear);
 			image.FlipY();
 			
-			image.SavePng("user://render/cs_" + framesDrawn + ".png");
+			image.SavePng(_outputDir + "/cs_" + framesDrawn.ToString("D8") + ".png");
 		}
+		
+		GD.Print("Rendered Frame " + framesDrawn.ToString("D8") + "..." );
 	}
 
-	public void _on_animation_player_animation_finished(String animationName) {
+	private void _on_AnimationPlayer_animation_finished(String anim_name)
+	{
 		GD.Print("Animation finished.");
 		GetTree().Quit();
 	}
